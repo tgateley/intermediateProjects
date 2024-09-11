be58b54dc443c4aa6f4c7cac7897e20ffa6844c7
Tiffany <tgateley22@gmail.com>
2024-09-11 00:13:36 -0500

    First commit

---
+++ grade_tracker.py
@@ -0,0 +1,23 @@
+grades = {}
+
+
+while True:
+    option = input("""Menu:
+    1. Add a student and their grades
+    2. View all students and their grades
+    3. Exit
+    Chose an option: """)
+
+    match option:
+        case "1":
+            student = input("Enter the student's name: ")
+            grade = input(f"Enter {student}'s grade: ")
+            grades[student] = grade
+            print(f"{student}'s grade has been addd")
+        case "2":
+            for name, grade in grades.items():
+                print(f"{name}: {grade}")
+        case "3":
+            break
+
+
